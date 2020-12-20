a8e9c22ad67636121d92e61656f9332f0031bbde
IbrahimShoukry512 <62100283+IbrahimShoukry512@users.noreply.github.com>
2020-12-20 10:47:33 +0200

    added activation function

--- ainshamsflow/activations.py
+++ ainshamsflow/activations.py
@@ -57,3 +57,74 @@ class LeakyReLU(Activation):
 
 	def diff(self, z):
 		return np.where(z > 0, 1, self.alpha)
+
+
+#todo : hard simgoid , selu , elu , soft sign , swish , softplus , hard tanh
+class Softplus(Activation):
+	def __call__(self,z):
+		return np.log( 1 + np.exp(z))
+	def diff(self,z):
+		return 1 / (1 + np.exp(-z))	# exp z / 1 + exp z
+
+class ELU(Activation):
+	def __init__(self,alpha_ELU=1.67326):
+		self.alpha_ELU = alpha_ELU
+	
+	def __call__(self,z):
+		return np.where(z <= 0 ,self.alpha_ELU * ( np.exp(z) - 1), z)
+	
+	def diff(self,z):
+		return np.where(z < 0 , self.alpha_ELU * np.exp(z) , 1) # alpha = ????
+
+class SELU(Activation):
+	def __init__(self ,alpha_SELU = 1.67326 , lambda_SELU =1.0507):
+		self.alpha_SELU = alpha_SELU
+		self.lambda_SELU = lambda_SELU
+	
+	def __call__(self,z):
+		return self.lambda_SELU * (np.where(z >= 0 , z ,self.alpha_SELU * ( np.exp(z) - 1) ))
+
+	def diff(self,z):
+		return self.lambda_SELU * (np.where(z >= 0 , 1 ,self.alpha_SELU * ( np.exp(z) )))
+
+class Softsign(Activation):
+	def __call__(self,z):
+		return z / ( 1 + np.abs(z))
+	
+	def diff(self,z):
+		Softsign_down = ( 1 + np.abs(z))
+		return 1 / np.power(Softsign_down  , 2)
+
+class Swish(Activation):
+	def __call__(self,z):
+		return z * Sigmoid(z)
+	
+	def diff(self,z):
+		return Sigmoid(z) + z * Sigmoid(z) * (1 - Sigmoid(z)) 
+
+class Softmax(Activation):
+	def __call__(self,z):
+		exp_z = np.exp(z)
+		sum = np.sum(exp_z,axis=-1,keepdims=True)
+		return exp_z / sum
+
+	def diff(self,z):
+		s = self.__call__(z)
+		return np.diagflat(s) - np.dot( s , np.transpose(s))
+
+class Hardtanh(Activation):
+	def __call__(self,z):
+		z = np.where(z >  1,  1, z)
+		z = np.where(z < -1, -1, z)
+		return z
+
+	def diff(self,z):
+		return np.where(np.logical_not(np.logical_or(np.greater(z,1),np.less(z,-1))) , 1 , 0)		
+
+class Hardsigmoid(Activation):
+	def __call__(self,z):
+		return np.maximum(0, np.minimum(1, (z + 1) / 2))
+
+	def diff(self,z):
+		return np.where(np.logical_not(np.logical_or( np.less(z, -2.5) , np.greater(z , 2.5) ), 0.2 , 0))		
+
