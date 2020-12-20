d35459cc1a6af516ddd820ed46637b52a76c121f
John-Bahaa <62305867+John-Bahaa@users.noreply.github.com>
2020-12-20 11:27:16 +0200

    add recgnalzter

--- ainshamsflow/regularizers.py
+++ ainshamsflow/regularizers.py
@@ -29,3 +29,51 @@ class L1(Regularizer):
 
 	def diff(self, weights_list, m):
 		return self.lambd * np.sum(np.where(weights_list > 0, 1, -1)) / m
+
+import numpy as np
+
+from ainshamsflow.utils.peras_errors import BaseClassError
+#TODO: Add More Regularizers
+
+
+class Regularizer:
+	def __init__(self, lambd):
+		self.lambd = lambd
+
+	def __call__(self, weights_list, m):
+		raise BaseClassError
+
+	def diff(self, weights_list, m):
+		raise BaseClassError
+
+
+class L2(Regularizer):
+	def __call__(self, weights_list, m):
+		return self.lambd * np.sum(np.square(weights_list)) / (2*m)
+
+	def diff(self, weights_list, m):
+		return self.lambd * np.sum(weights_list) / m
+
+
+class L1(Regularizer):
+	def __call__(self, weights_list, m):
+		return self.lambd * np.sum(np.abs(weights_list)) / m
+
+	def diff(self, weights_list, m):
+		return self.lambd * np.sum(np.where(weights_list > 0, 1, -1)) / m
+	
+	
+class L1L2(Regularizer):
+        def __call__(self, weights_list, m):
+          regularization = backend.constant(0., dtype=x.dtype)
+          if self.L1:
+            regularization += self.lambd * np.sum(np.abs(weights_list)) / m
+          if self.L2:
+            regularization += self.lambd * np.sum(np.square(weights_list)) / (2*m)
+          return regularization
+
+        def diff(self, weights_list, m):
+	  if self.L1:
+          return self.lambd * np.sum(np.where(weights_list > 0, 1, -1)) / m
+          if self.L2:
+          return self.lambd * np.sum(weights_list) / m
