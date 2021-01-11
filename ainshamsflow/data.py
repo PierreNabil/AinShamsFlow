35b043abfa5fcd403dfe4cd26af6e58e59636937
Your Name <you@example.com>
2021-01-11 21:44:05 +0200

    "Functions: take,map,batch are done!"

--- ainshamsflow/data.py
+++ ainshamsflow/data.py
@@ -2,12 +2,19 @@ import numpy as np
 
 
 class Dataset:
-	def __init__(self, iterator):
-		#dimentions: (batch, m, n)
-		self.data = np.array(iterator).T
+	def __init__(self, x=None, y=None):
+		if x is not None:
+			self.data = np.array(x)
+		else:
+			self.data = None
+
+		if y is not None:
+			self.target = np.array(y)
+		else:
+			self.target = None
 
 	def __str__(self):
-		return str(self.data.T)
+		pass
 
 	def __bool__(self):
 		pass
@@ -20,23 +27,43 @@ class Dataset:
 		return self
 
 	def __next__(self):
-		if self.index > np.data.shape[0]:
+		assert self.data is not None
+		if self.index >= self.data.shape[0]:
 			raise StopIteration
-		batch = np.data[self.index].T
-		self.index += 1
-		return batch
 
+		if self.data is not None and self.target is not None:
+			x = self.data[self.index]
+			y = self.target[self.index]
+			self.index += 1
+			return x, y
+
+		elif self.data is not None:
+			x = self.data[self.index]
+			self.index += 1
+			return x
 	def apply(self):
 		pass
 
 	def numpy(self):
 		pass
 
-	def batch(self):
-		pass
-
+	def batch(self,batch_size,drop_reminder=True):
+		i = 0
+		dt = list()
+		targ - list()
+		sz = len(self.data)
+		if drop_reminder==True:
+			sz-=sz%batch_size
+		while i < sz:
+			t=i
+			i+=batch_size
+			dt.append(self.data[t:i])
+			targ.append(self.target[t:i])
+		self.data=np.array(dt)
+		self.target=np.array(targ)
+        
 	def cardinality(self):
-		pass
+		pass 
 
 	def concatenate(self):
 		pass
@@ -47,8 +74,12 @@ class Dataset:
 	def filter(self):
 		pass
 
-	def map(self):
-		pass
+	def map(self,func):
+		ds=[]
+		for element in self.data:
+			ds.append(func(element))
+		self.data=np.array(ds)
+
 
 	def range(self):
 		pass
@@ -62,13 +93,17 @@ class Dataset:
 	def skip(self):
 		pass
 
-	def take(self):
-		pass
+	def take(self,limit):
+		return self.data[:limit]
+		
 
 	def unbatch(self):
 		pass
 
 	def zip(self):
+		pass
+	
+	def normalize(self):
 		pass
 
 
