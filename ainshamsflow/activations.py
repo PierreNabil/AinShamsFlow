fixed softmax and crossentropy bug

ainshamsflow/activations.py
@@ -162,15 +162,15 @@ class Softmax(Activation):
 		return ans
 
 	def diff(self, z):
-		da = np.ones(z.shape)
-		m, n = z.shape
-		p = self.__call__(z)
-		tensor1 = np.einsum('ij,ik->ijk', p, p)  # (m, n, n)
-		tensor2 = np.einsum('ij,jk->ijk', p, np.eye(n, n))  # (m, n, n)
-		dz = tensor2 - tensor1
-		dz = np.einsum('ijk,ik->ij', dz, da)
-
-		return dz
+		# da = np.ones(z.shape)
+		# m, n = z.shape
+		# p = self.__call__(z)
+		# tensor1 = np.einsum('ij,ik->ijk', p, p)  # (m, n, n)
+		# tensor2 = np.einsum('ij,jk->ijk', p, np.eye(n, n))  # (m, n, n)
+		# dz = tensor2 - tensor1
+		# dz = np.einsum('ijk,ik->ij', dz, da)
+		# return dz
+		return 1
 
 
 class HardTanh(Activation):

ainshamsflow/losses.py
@@ -41,10 +41,12 @@ class MSE(Loss):
     __name__ = 'MSE'
 
     def __call__(self, y_pred, y_true):
-        return np.mean(np.square(y_pred - y_true)) / 2
+        m = y_pred.shape[0]
+        return np.sum(np.square(y_pred - y_true)) / (2*m)
 
     def diff(self, y_pred, y_true):
-        return np.mean(y_pred - y_true, axis=0, keepdims=True)
+        m = y_pred.shape[0]
+        return (y_pred - y_true) / m
 
 
 class MAE(Loss):
@@ -52,7 +54,8 @@ class MAE(Loss):
     __name__ = 'MAE'
 
     def __call__(self, y_pred, y_true):
-        return np.mean(np.abs(y_pred - y_true))
+        m = y_true.shape[0]
+        return np.sum(np.abs(y_pred - y_true)) / m
 
     def diff(self, y_pred, y_true):
         m = y_true.shape[0]
@@ -66,7 +69,7 @@ class HuberLoss(Loss):
     def __init__(self, delta=1.0):
         self.delta = delta
 
-    def __call__(self, y_true, y_pred):
+    def __call__(self, y_pred, y_true):
         return np.where(np.abs(y_pred - y_true) <= self.delta, 0.5 * np.square(y_pred - y_true),
                         (self.delta * np.abs(y_pred - y_true)) - 0.5 * np.square(self.delta))
 
@@ -79,7 +82,8 @@ class MAPE(Loss):
     __name__ = 'MAPE'
 
     def __call__(self, y_pred, y_true):
-        return np.mean(np.abs(y_pred - y_true) / y_true)
+        m = y_pred.sahpe[0]
+        return np.sum(np.abs(y_pred - y_true) / y_true) / m
 
     def diff(self, y_pred, y_true):
         m = y_true.shape[0]
@@ -90,7 +94,7 @@ class LogLossLinear(Loss):
     """Logistic loss in case of identity(linear) activation function"""
     __name__ = "LogLossLinear"
 
-    def __call__(self, y_true, y_pred):
+    def __call__(self, y_pred, y_true):
         return np.sum(np.log(1 + np.exp(-y_true * y_pred)))
 
     def diff(self, y_pred, y_true):
@@ -101,7 +105,8 @@ class LogLossSigmoid(Loss):
     __name__ = "LogLossSigmoid"
 
     def __call__(self, y_pred, y_true):
-        return -np.mean(np.log(np.abs(y_true / 2 - 0.5 + y_pred)))
+        m = y_pred.shape[0]
+        return -np.sum(np.log(np.abs(y_true / 2 - 0.5 + y_pred))) / m
 
     def diff(self, y_pred, y_true):
         x = y_true / 2 - 0.5 + y_pred
@@ -111,7 +116,7 @@ class LogLossSigmoid(Loss):
 class PerceptronCriterion(Loss):
     __name__ = 'PerceptronCriterion'
 
-    def __call__(self, y_true, y_pred):
+    def __call__(self, y_pred, y_true):
         return np.maximum(0, -y_true * y_pred)
 
     def diff(self, y_pred, y_true):
@@ -132,23 +137,23 @@ class BinaryCrossentropy(Loss):
     __name__ = 'BinaryCrossentropy'
 
     def __call__(self, y_pred, y_true):
-        return -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
+        m = y_pred.shape[0]
+        return -np.sum(np.where(y_true, np.log(y_pred), np.log(1 - y_pred))) / m
 
     def diff(self, y_pred, y_true):
-        # m = y_true.shape[0]
-        # return -np.sum(((y_true / y_pred) - (1 - y_true / 1 - y_pred))) / m
-        np.mean(y_pred - y_true, axis=0, keepdims=True)
+        m = y_pred.shape[0]
+        return (y_pred - y_true) / m
 
 
 class CategoricalCrossentropy(Loss):
     __name__ = 'CategoricalCrossentropy'
 
-    def __call__(self, y_true, y_pred):
-        return -np.mean(y_true * np.log(y_pred + 1e-6))
+    def __call__(self, y_pred, y_true):
+        return -np.mean(np.log(np.max(y_true * y_pred, axis=1) + 1e-6))
 
     def diff(self, y_pred, y_true):
         m = y_true.shape[0]
-        return -(y_true / y_pred) / m
+        return (y_pred - y_true) / m
 
 
 class SparseCategoricalCrossentropy(Loss):
@@ -157,10 +162,10 @@ class SparseCategoricalCrossentropy(Loss):
     def __call__(self, y_pred, y_true):
         n_c = y_pred.shape[-1]
         y_true = true_one_hot(y_true, n_c)
-        return -np.mean(y_true * np.log(y_pred + 1e-6))
+        return -np.mean(np.log(np.max(y_true * y_pred, axis=1) + 1e-6))
 
     def diff(self, y_pred, y_true):
+        m = y_pred.shape[0]
         n_c = y_pred.shape[-1]
         y_true = true_one_hot(y_true, n_c)
-        m = y_pred.shape[0]
-        return -(y_true / y_pred) / m
+        return (y_pred - y_true) / m

ainshamsflow/optimizers.py
@@ -45,8 +45,8 @@ class Optimizer:
         rem_batch_size = m - batch_size * num_of_batches
         history = History(loss, metrics)
         if verbose:
-            print()
-            print()
+            # print()
+            # print()
             if training:
                 print('Training Model for {} epochs:'.format(epochs))
             else:
@@ -73,12 +73,14 @@ class Optimizer:
                 loss_values.append(loss_value)
                 metrics_values.append(metric_values)
             loss_values = np.array(loss_values).sum()
-            metrics_values = np.array(metrics_values).mean(axis=1)
+            metrics_values = np.array(metrics_values)
+            if metrics:
+                metric_values = metrics_values.mean(axis=1)
             history.add(loss_values, metrics_values)
             if verbose:
                 if training:
                     print(
-                        'Finished epoch number {:4d}:'.format(epoch_num),
+                        'Epoch #{:4d}:'.format(epoch_num+1),
                         '{}={:8.4f},'.format(loss.__name__, loss_values),
                         *['{}={:8.4f},'.format(metric.__name__, metrics_values[j]) for j, metric in enumerate(metrics)]
                     )

main.py
@@ -4,14 +4,8 @@ import ainshamsflow as asf
 import numpy as np
 
 
-def _true_one_hot(y_true):
-	n_c = np.max(y_true) + 1
-	return np.squeeze(np.eye(n_c)[y_true])
-
-
-x = np.random.rand(15, 10, 10, 3)
-y = np.random.randint(0, 5, (15, 1))
-y = _true_one_hot(y)
+x = np.random.rand(10000, 10, 10, 3)
+y = np.random.randint(0, 5, (10000, 1))
 # print(x[0,:,:,0], y[0])
 print(x.shape, y.shape)
 
@@ -22,26 +16,18 @@ model = asf.models.Sequential([
 	asf.layers.Flatten(),
 	asf.layers.Dense(100, activation='relu'),
 	asf.layers.Dense( 30, activation='relu'),
-	asf.layers.Dense( 5, activation='softmax')
+	asf.layers.Dense(  5, activation='softmax')
 ], input_shape=(10, 10, 3), name='my_model')
 
 model.print_summary()
 
 model.compile(
-	asf.optimizers.SGD(lr=0.01),
-	asf.losses.CategoricalCrossentropy()
+	asf.optimizers.AdaGrad(lr=0.1),
+	asf.losses.SparseCategoricalCrossentropy()
 )
 
-history = model.fit(x, y, 100)
+history = model.fit(x, y, 50)
 model.evaluate(x, y)
 model.evaluate(x, y)
 
 history.show()
-
-
-# x_test = np.array([[1, 1, 1]])
-# y_test = np.array([[10]])
-#
-# print(model.predict(x_test))
-#
-# model.evaluate(x_test, y_test)