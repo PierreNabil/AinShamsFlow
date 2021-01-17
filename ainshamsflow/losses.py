Fixed softmax bug.

ainshamsflow/losses.py
@@ -142,7 +142,7 @@ class BinaryCrossentropy(Loss):
 
     def diff(self, y_pred, y_true):
         m = y_pred.shape[0]
-        return (y_pred - y_true) / m
+        return (y_pred - y_true)
 
 
 class CategoricalCrossentropy(Loss):
@@ -153,7 +153,7 @@ class CategoricalCrossentropy(Loss):
 
     def diff(self, y_pred, y_true):
         m = y_true.shape[0]
-        return (y_pred - y_true) / m
+        return (y_pred - y_true)
 
 
 class SparseCategoricalCrossentropy(Loss):
@@ -162,10 +162,10 @@ class SparseCategoricalCrossentropy(Loss):
     def __call__(self, y_pred, y_true):
         n_c = y_pred.shape[-1]
         y_true = true_one_hot(y_true, n_c)
-        return -np.mean(np.log(np.max(y_true * y_pred, axis=1) + 1e-6))
+        return -np.mean(np.log(np.max(y_true * y_pred, axis=1)))
 
     def diff(self, y_pred, y_true):
         m = y_pred.shape[0]
         n_c = y_pred.shape[-1]
         y_true = true_one_hot(y_true, n_c)
-        return (y_pred - y_true) / m
+        return (y_pred - y_true)

ainshamsflow/metrics.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from ainshamsflow.utils.utils import pred_one_hot, true_one_hot
+from ainshamsflow.utils.utils import pred_one_hot, true_one_hot, confution_matrix
 from ainshamsflow.utils.asf_errors import BaseClassError, NameNotFoundError, UnsupportedShapeError
 
 
@@ -23,80 +23,88 @@ def _check_dims(y_pred, y_true):
 
 
 class Metric:
-	def __call__(self, y_pred, y_true):
+	def __call__(self):
 		raise BaseClassError
+
+	@classmethod
+	def calc_confusion_matrix(cls, y_pred, y_true):
+		y_true = _check_dims(y_pred, y_true)
+		y_pred = pred_one_hot(y_pred)
+		cls.conf_mtx = confution_matrix(y_pred, y_true)
+		cls.m = y_pred.shape[0]
 
 
 class FalseNegative(Metric):
 	__name__ = 'FN'
 
-	def __call__(self, y_pred, y_true):
-		y_true = _check_dims(y_pred, y_true)
-		y_pred = pred_one_hot(y_pred)
-		return np.sum(np.logical_and(y_pred == 0, y_true == 1))
+	def class_wise(self):
+		return np.sum(self.conf_mtx, axis=1) - TruePositive().class_wise()
+
+	def __call__(self):
+		return np.sum(self.class_wise())
 
 
 class FalsePositive(Metric):
 	__name__ = 'FP'
 
-	def __call__(self, y_pred, y_true):
-		y_true = _check_dims(y_pred, y_true)
-		y_pred = pred_one_hot(y_pred)
-		return np.sum(np.logical_and(y_pred == 1, y_true == 0))
+	def class_wise(self):
+		return np.sum(self.conf_mtx, axis=0) - TruePositive().class_wise()
+
+	def __call__(self):
+		return np.sum(self.class_wise())
 
 
 class TrueNegative(Metric):
 	__name__ = 'TN'
 
-	def __call__(self, y_pred, y_true):
-		y_true = _check_dims(y_pred, y_true)
-		y_pred = pred_one_hot(y_pred)
-		return np.sum(np.logical_and(y_pred == 0, y_true == 0))
+	def class_wise(self):
+		n = self.conf_mtx.shape[0]
+		return np.array([np.sum(np.delete(np.delete(self.conf_mtx, i, 0), i, 1)) for i in range(n)])
+
+	def __call__(self):
+		return np.sum(self.class_wise())
 
 
 class TruePositive(Metric):
 	__name__ = 'TP'
 
-	def __call__(self, y_pred, y_true):
-		y_true = _check_dims(y_pred, y_true)
-		y_pred = pred_one_hot(y_pred)
-		return np.sum(np.logical_and(y_pred == 1, y_true == 1))
+	def class_wise(self):
+		n = self.conf_mtx.shape[0]
+		return np.array([self.conf_mtx[i][i] for i in range(n)])
+
+	def __call__(self):
+		return np.sum(self.class_wise())
 
 
 class Accuracy(Metric):
 	__name__ = 'Accuracy'
 
-	def __call__(self, y_pred, y_true):
-		TP = TruePositive()(y_pred, y_true)
-		TN = TrueNegative()(y_pred, y_true)
-		FP = FalsePositive()(y_pred, y_true)
-		FN = FalseNegative()(y_pred, y_true)
-		return (TP + TN) / (TP + TN + FP + FN)
+	def __call__(self):
+		return TruePositive()() / self.m
 
 
 class Precision(Metric):
 	__name__ = 'Precision'
 
-	def __call__(self, y_pred, y_true):
-		TP = TruePositive()(y_pred, y_true)
-		FP = FalsePositive()(y_pred, y_true)
-		return TP/(TP+FP)
+	def __call__(self):
+		TP = TruePositive().class_wise()
+		FP = FalsePositive().class_wise()
+		return np.mean(np.where(np.logical_and(TP == 0, FP == 0), 0, TP/(TP+FP)))
 
 
 class Recall(Metric):
 	__name__ = 'Recall'
 
-	def __call__(self, y_pred, y_true):
-		TP = TruePositive()(y_pred, y_true)
-		FN = FalseNegative()(y_pred, y_true)
-		return TP/(TP+FN)
+	def __call__(self):
+		TP = TruePositive().class_wise()
+		FN = FalseNegative().class_wise()
+		return np.mean(np.where(np.logical_and(TP == 0, FN == 0), 0, TP/(TP+FN)))
 
 
 class F1Score(Metric):
 	__name__ = 'F1Score'
 
-	def __call__(self, y_pred, y_true):
-		TP = TruePositive()(y_pred, y_true)
-		FN = FalseNegative()(y_pred, y_true)
-		FP = FalsePositive()(y_pred, y_true)
-		return 2*TP/(2*TP+FP+FN)
+	def __call__(self):
+		p = Precision()()
+		r = Recall()()
+		return 2 / (1/p + 1/r)

ainshamsflow/models.py
@@ -154,32 +154,28 @@ class Sequential(Model):
 			input_shape = layer.add_input_shape_to_layers(input_shape)
 
 		self.n_out = self.layers[-1].n_out
-		# print(self.n_out)
 		n_out = [str(ch) for ch in self.n_out]
 		self.output_shape = '(None' + (',{:4}'*len(n_out)).format(*n_out) + ')'
 
-	def fit(self, x, y=None, epochs=1, batch_size=None, verbose=True) :
+	def fit(self, x, y=None, epochs=1, batch_size=None, verbose=True):
 		"""Fit the model to the training data."""
 
 		ds = get_dataset_from_xy(x, y)
 
-		if self.optimizer is None :
+		if self.optimizer is None:
 			raise UncompiledModelError
 		return self.optimizer(ds, epochs, batch_size, self.layers, self.loss, self.metrics, self.regularizer,
 							  verbose=verbose, training=True)
 
-	def evaluate(self, x, y=None, batch_size=None, verbose=True) :
+	def evaluate(self, x, y=None, batch_size=None, verbose=True):
 		"""Evaluate the model on validation data."""
 
 		ds = get_dataset_from_xy(x, y)
 
-		if self.optimizer is None :
+		if self.optimizer is None:
 			raise UncompiledModelError
-		history = self.optimizer(ds, 1, batch_size, self.layers, self.loss, self.metrics, self.regularizer,
+		return self.optimizer(ds, 1, batch_size, self.layers, self.loss, self.metrics, self.regularizer,
 								 verbose=verbose, training=False)
-		loss_value = np.mean(history.loss_values)
-		metric_values = history.flipped_metrics()
-		return loss_value, metric_values
 
 	def add_layer(self, layer):
 		"""Add a new layer to the network."""

ainshamsflow/optimizers.py
@@ -6,6 +6,7 @@ Stocastic Gradient Descent (SGD).
 
 import numpy as np
 
+from ainshamsflow.metrics import Metric
 from ainshamsflow.utils.asf_errors import BaseClassError, NameNotFoundError, UnsupportedShapeError
 from ainshamsflow.utils.history import History
 
@@ -51,11 +52,43 @@ class Optimizer:
             ds.batch(batch_size)
 
         history = History(loss, metrics)
+        loss_values = []
+        metrics_values = []
+
         if verbose:
             if training:
                 print('Training Model for {} epochs:'.format(epochs))
             else:
                 print('Evaluating Model:')
+
+        for i, (batch_x, batch_y) in enumerate(ds):
+            loss_value, metric_values = self._single_iteration(batch_x, batch_y, m, layers,
+                                                               loss, metrics, regularizer, False, i)
+            loss_values.append(loss_value)
+            metrics_values.append(metric_values)
+
+        loss_values = np.array(loss_values).sum()
+        metrics_values = np.array(metrics_values).mean(axis=0)
+
+        if verbose:
+            if training:
+                print(
+                    'Epoch #{:4d}:'.format(0),
+                    '{}={:8.4f},'.format(loss.__name__, loss_values),
+                    *['{}={:8.4f},'.format(metric.__name__, metrics_values[j]) for j, metric in enumerate(metrics)]
+                )
+                history.add(loss_values, metrics_values)
+            else:
+                print(
+                    '{}={:8.4f},'.format(loss.__name__, loss_values),
+                    *['{}={:8.4f},'.format(metric.__name__, metrics_values[j]) for j, metric in enumerate(metrics)]
+                )
+
+        if not training:
+            if verbose:
+                return
+            else:
+                return loss_values, metrics_values
 
         for epoch_num in range(epochs):
             loss_values = []
@@ -70,18 +103,11 @@ class Optimizer:
             metrics_values = np.array(metrics_values).mean(axis=0)
             history.add(loss_values, metrics_values)
             if verbose:
-                if training:
-                    print(
-                        'Epoch #{:4d}:'.format(epoch_num+1),
-                        '{}={:8.4f},'.format(loss.__name__, loss_values),
-                        *['{}={:8.4f},'.format(metric.__name__, metrics_values[j]) for j, metric in enumerate(metrics)]
-                    )
-                else:
-                    print(
-                        '{}={:8.4f},'.format(loss.__name__, loss_values),
-                        *['{}={:8.4f},'.format(metric.__name__, metrics_values[j]) for j, metric in enumerate(metrics)]
-
-                    )
+                print(
+                    'Epoch #{:4d}:'.format(epoch_num+1),
+                    '{}={:8.4f},'.format(loss.__name__, loss_values),
+                    *['{}={:8.4f},'.format(metric.__name__, metrics_values[j]) for j, metric in enumerate(metrics)]
+                )
 
         return history
 
@@ -96,8 +122,10 @@ class Optimizer:
         regularization_term = 0 if regularizer is None else regularizer(weights_list, m)
         loss_value = loss(batch_a, batch_y) + regularization_term
         metric_values = []
-        for metric in metrics:
-            metric_values.append(metric(batch_a, batch_y))
+        if metrics:
+            Metric.calc_confusion_matrix(batch_a, batch_y)
+            for metric in metrics:
+                metric_values.append(metric())
         # Backward Pass
         regularization_diff = None if regularizer is None else regularizer.diff(weights_list, m)
         da = loss.diff(batch_a, batch_y)

ainshamsflow/utils/history.py
@@ -45,7 +45,7 @@ class History:
 		fig, axs = plt.subplots(num_of_plots, 1, squeeze=False, figsize=(8, 3*num_of_plots))
 
 		# Loss Plots:
-		epochs = np.arange(len(self.loss_values)) + 1
+		epochs = np.arange(len(self.loss_values))
 		axs[0, 0].plot(epochs, self.loss_values)
 		axs[0, 0].set_title('Model Loss')
 		axs[0, 0].set_ylabel(self.loss_name)
@@ -61,5 +61,9 @@ class History:
 					axs[j+1, 0].set_title('Model Metrics')
 				axs[j+1, 0].set_ylabel(self.metric_names[j])
 				axs[j+1, 0].set_xlabel('epochs')
-				axs[j+1, 0].set_ylim(0, 1)
+				if len(self.metric_names[j]) == 2:
+					bottom, top = axs[j+1, 0].get_ylim()
+					axs[j+1, 0].set_ylim(0, top)
+				else:
+					axs[j+1, 0].set_ylim(0, 1)
 		plt.show()

main.py
@@ -1,45 +1,62 @@
 """Used to test all created objects."""
 
 import ainshamsflow as asf
-import numpy as np
+import pandas as pd
 
 
-x = np.random.rand(15, 10, 10, 3)
-y = np.random.randint(0, 5, (15, 1))
+# K_train = pd.read_csv('../train.csv').to_numpy()
+# # ds_split = 40000
+# ds_split = 100
+# X_train = K_train[:ds_split, 1:].reshape((-1, 28, 28, 1)) / 255
+# X_valid = K_train[ds_split:, 1:].reshape((-1, 28, 28, 1)) / 255
+# y_train = K_train[:ds_split, :1].astype('int')
+# y_valid = K_train[ds_split:, :1].astype('int')
+#
+# ds_train = asf.data.Dataset(X_train, y_train)
+# ds_valid = asf.data.Dataset(X_valid, y_valid)
 
-ds = asf.data.Dataset(x,y)
-ds.batch(3)
 
-conv_part = asf.models.Sequential([
-	asf.layers.Conv2D(5, 3, padding='same', activation='relu'),
-	asf.layers.Pool2D(2),
-	asf.layers.Flatten()
-], input_shape=(10, 10, 3), name='conv_part')
+K_train = pd.read_csv('../train.csv').to_numpy()
+# ds_split = 40000
+ds_split = 40000
+X_train = K_train[:ds_split, 1:] / 255
+X_valid = K_train[ds_split:, 1:] / 255
+y_train = K_train[:ds_split, :1].astype('int')
+y_valid = K_train[ds_split:, :1].astype('int')
 
-conv_part.print_summary()
+ds_train = asf.data.Dataset(X_train, y_train)
+ds_valid = asf.data.Dataset(X_valid, y_valid)
 
+
+# model = asf.models.Sequential([
+# 	asf.layers.Conv2D( 8, kernel_size=5),
+# 	asf.layers.Pool2D( 2, mode='avg'),
+# 	asf.layers.Conv2D(16, kernel_size=5),
+# 	asf.layers.Pool2D( 2, mode='avg'),
+# 	asf.layers.Flatten(),
+# 	asf.layers.Dense(120, activation='relu'),
+# 	asf.layers.Dense( 84, activation='relu'),
+# 	asf.layers.Dense( 10, activation='softmax')
+# ], input_shape=(28, 28, 1), name='LeNet5_model')
 
 model = asf.models.Sequential([
-	conv_part,
-	asf.layers.Dense(100, activation='relu'),
-	asf.layers.Dense( 30, activation='relu'),
-	asf.layers.Dense(  5, activation='softmax')
-], input_shape=(10, 10, 3), name='my_model')
+	asf.layers.Dense(300, 'relu'),
+	asf.layers.Dense(100, 'relu'),
+	asf.layers.Dense(10, 'softmax')
+], input_shape=(28*28,), name='DNN_model')
 
 model.print_summary()
 
 model.compile(
-	asf.optimizers.AdaGrad(lr=0.1),
-	'SparseCategoricalCrossentropy',
-	['accuracy', 'precision', 'recall', 'f1score'],
-	'l1'
+	asf.optimizers.SGD(lr=0.01),
+	'sparsecategoricalcrossentropy',
+	['accuracy']
 )
 
 history = model.fit(
-	ds,
-	epochs=50,
-	batch_size=4
+	ds_train,
+	epochs=20
 )
-model.evaluate(ds)
-
 history.show()
+
+model.evaluate(ds_valid)